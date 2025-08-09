d4f4708559cdf7e99f49bf81aabdb82db0af0bc0
Yuhan Wang <1048429491@qq.com>
2025-08-09 16:00:08 +0800

    fix pocl uninit not-called problem
    This was a intentional selection made by pocl, but we need `uninit` to call vt_dev_close, so that verilator can generate corrent FST waveform file.
    
    See https://pcn2po10nqam.feishu.cn/wiki/XHNXwIdRkiFtZDkCW6Mc58Uon3b

--- env.sh
+++ env.sh
@@ -5,6 +5,9 @@ export LD_LIBRARY_PATH=${VENTUS_INSTALL_PREFIX}/lib:${LD_LIBRARY_PATH:-}
 export POCL_DEVICES="ventus"
 export OCL_ICD_VENDORS=${VENTUS_INSTALL_PREFIX}/lib/libpocl.so
 
+# see https://pcn2po10nqam.feishu.cn/wiki/XHNXwIdRkiFtZDkCW6Mc58Uon3b
+export POCL_ENABLE_UNINIT=1
+
 # remove extra colons
 export LD_LIBRARY_PATH=$(echo "$LD_LIBRARY_PATH" | sed -e 's/^:*//' -e 's/:*$//')
 
